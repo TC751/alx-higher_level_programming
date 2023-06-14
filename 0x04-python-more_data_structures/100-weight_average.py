5bb466cd93397da47ebf1592846d89e703f6ef07
TC751 <teddychiingujr@gmail.com>
2023-06-14 08:52:00 -0700

    11-multiply_list_map.py redone

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,12 +4,10 @@ def weight_average(my_list=[]):
         return 0
 
     num = 0
-    dens = 0
+    den = 0
 
     for tup in my_list:
         num += tup[0] * tup[1]
-        dens += tup[1]
+        den += tup[1]
 
-    return (num / dens)
-	
-
+    return (num / den)
