bc91892a3d3d0ca55930784c0fb95c00a6c8b7fc
TC751 <teddychiingujr@gmail.com>
2023-08-16 07:16:34 -0700

    0x0E-SQL_more_queries files

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Script that lists all the cities of California registered in the database
+SELECT id, name -- Query to list all the cities from California
+FROM cities
+ORDER BY cities.id ASC
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California"
+)
+ORDER BY id ASC;
